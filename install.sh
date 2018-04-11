47d17a7be600f0b40a98de78bd897451068c195e
zuladev <abdurahman@live.dk>
2018-04-11 13:52:54 +0300

    port fix

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ cp /root/temp/zula-cli /root/zula
 cd /root/zula
 sudo apt-get install -y pwgen
 GEN_PASS=`pwgen -1 20 -n`
-echo -e "rpcuser=zulauser\nrpcpassword=${GEN_PASS}\nrpcport=12500\nport=12501\nlisten=1\nmaxconnections=256" > /root/.zulacore/zula.conf
+echo -e "rpcuser=zulauser\nrpcpassword=${GEN_PASS}\nrpcport=12501\nport=12500\nlisten=1\nmaxconnections=256" > /root/.zulacore/zula.conf
 cd /root/zula
 ./zulad -daemon
 sleep 10
